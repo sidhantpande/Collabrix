6d59438614a28031f69a6af6153e9551bd12228c
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-03-10 15:57:01 -0300

    feat(auth): add authentication service with signup and login

---
+++ auth-service/src/main/java/com/mobflow/authservice/services/AuthenticationService.java
@@ -0,0 +1,51 @@
+package com.mobflow.authservice.services;
+
+import com.mobflow.authservice.domain.model.dtos.LoginUserDTO;
+import com.mobflow.authservice.domain.model.dtos.RegisterUserCredentialsDTO;
+import com.mobflow.authservice.domain.model.entities.UserCredential;
+import com.mobflow.authservice.domain.repository.UserCredentialRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    private final UserCredentialRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(
+            UserCredentialRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserCredential signup(RegisterUserCredentialsDTO input) {
+        UserCredential user = UserCredential.builder()
+                .username(input.getUsername())
+                .email(input.getEmail())
+                .passwordHash(passwordEncoder.encode(input.getPassword()))
+                .build();
+        return userRepository.save(user);
+    }
+
+    public UserCredential authenticate(LoginUserDTO input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        return userRepository.findByEmail(input.getEmail())
+                .orElseThrow();
+    }
+}
