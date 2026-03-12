eaff6fe4bc8cd9cbc97162849f76dd64db7893b8
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-03-12 11:14:19 -0300

    refactor(dto): add Builder pattern to UserResponseDTO

--- auth-service/src/main/java/com/mobflow/authservice/domain/model/dtos/UserResponseDTO.java
+++ auth-service/src/main/java/com/mobflow/authservice/domain/model/dtos/UserResponseDTO.java
@@ -1,8 +1,9 @@
 package com.mobflow.authservice.domain.model.dtos;
-
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UserResponseDTO {
     private Long id;
     private String username;
