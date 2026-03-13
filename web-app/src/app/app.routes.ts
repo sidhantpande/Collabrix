0797402ce32fad2748de5d0dc3c5f86cedbdcecb
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-03-13 18:36:05 -0300

    fix(web-app): minor fix and import to exception system work

--- web-app/src/app/app.routes.ts
+++ web-app/src/app/app.routes.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
-import { RegisterComponent } from './features/auth/pages/register.component';
+import { RegisterComponent } from './features/auth/pages/register/register.component';
 
 export const routes: Routes = [{ path: 'signup', component: RegisterComponent }];
--- web-app/src/app/app.ts
+++ web-app/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ExceptionComponent } from './shared/components/alert/exception.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ExceptionComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('web-app');
