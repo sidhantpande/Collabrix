7c4e3ba79ac2e7781c74cb5ece7b1ec937e926af
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-03-17 16:23:22 -0300

    feat(web-app): add dashboard route

--- web-app/src/app/app.routes.ts
+++ web-app/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/auth/pages/register/register.component';
 import { LoginComponent } from './features/auth/pages/login/login.component';
+import { DashboardComponent } from './features/auth/pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: 'signup', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
