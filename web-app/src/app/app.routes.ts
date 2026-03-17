ebe7c1fa4a6ede5f30f88a5c86d7c751cbd58888
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-03-17 17:14:18 -0300

    feat(web-app): apply guest guard to login and signup routes

--- web-app/src/app/app.routes.ts
+++ web-app/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/auth/pages/register/register.component';
 import { LoginComponent } from './features/auth/pages/login/login.component';
 import { DashboardComponent } from './features/auth/pages/dashboard/dashboard.component';
+import { guestGuard } from './core/guards/guest.guard';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: RegisterComponent },
+  { path: 'login', component: LoginComponent, canActivate: [guestGuard] },
+  { path: 'signup', component: RegisterComponent, canActivate: [guestGuard] },
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
