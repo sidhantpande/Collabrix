de52bccea4995295e5beaa147b389ec23ec9da79
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-03-17 17:55:06 -0300

    refactor(web-app): register navbar in app root and remove test alert buttons

--- web-app/src/app/app.ts
+++ web-app/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AlertComponent } from './shared/components/alert/alert.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AlertComponent],
+  imports: [RouterOutlet, AlertComponent, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
