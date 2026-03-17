fa8b8d18ddd40df2b87dbd09d0255a053ebf155b
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-03-17 16:36:46 -0300

    refactor(web-app):unuse imports remove

--- web-app/src/app/app.ts
+++ web-app/src/app/app.ts
@@ -1,8 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AlertComponent } from './shared/components/alert/alert.component';
-import { AlertService } from './shared/components/alert/service/alert.service';
-import { AlertType } from './shared/components/alert/enum/alert-type.enum';
 
 @Component({
   selector: 'app-root',
