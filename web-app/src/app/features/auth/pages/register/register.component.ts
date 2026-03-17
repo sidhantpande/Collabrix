3cabf306b4327f8339c1624689c587c227f5cc15
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-03-17 15:28:11 -0300

    fix(web-app): remove unused imports from register component

--- web-app/src/app/features/auth/pages/register/register.component.ts
+++ web-app/src/app/features/auth/pages/register/register.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../../../core/services/auth.service';
 import { SignupRequest } from '../../../../core/models/auth.model';
-import { ExceptionService } from '../../../../core/services/exception.service';
-import { ErrorResponseDTO } from '../../../../core/models/exception.model';
-import { ExceptionComponent } from '../../../../shared/components/alert/exception.component';
+import { ErrorHandlerService } from '../../../../core/services/error-handler.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    ExceptionComponent,
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
@@ -23,7 +17,7 @@ export class RegisterComponent {
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private exceptionService: ExceptionService,
+    private exceptionService: ErrorHandlerService,
   ) {
     this.registerForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(3)]],
@@ -35,24 +29,7 @@ export class RegisterComponent {
   onSubmit() {
     if (this.registerForm.valid) {
       const payload = this.registerForm.value as SignupRequest;
-
-      this.authService.register(payload).subscribe({
-        next: (response) => {
-          // Opcional: mostrar sucesso
-          this.exceptionService.showAlert('Cadastro realizado!', 'success', 'Sucesso');
-        },
-        error: (err) => {
-          // 2. Extrair o DTO que veio do seu Spring Boot
-          const errorBody: ErrorResponseDTO = err.error;
-
-          if (errorBody && errorBody.errorType) {
-            // 3. Chamar a lógica que traduz o enum (EMAIL_ALREADY_EXIST, etc)
-            this.exceptionService.handleHttpError(errorBody.errorType);
-          } else {
-            this.exceptionService.showAlert('Erro inesperado.', 'danger', 'Erro');
-          }
-        },
-      });
+      this.authService.register(payload).subscribe({});
     }
   }
 }
