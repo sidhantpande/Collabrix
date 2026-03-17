31e4be30b059beaa6e21c560afa7f59a17f38adb
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-03-17 15:46:49 -0300

    feat(web-app): add login route and default redirect to login

--- web-app/src/app/app.routes.ts
+++ web-app/src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/auth/pages/register/register.component';
+import { LoginComponent } from './features/auth/pages/login/login.component';
 
-export const routes: Routes = [{ path: 'signup', component: RegisterComponent }];
+export const routes: Routes = [
+  { path: 'signup', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
