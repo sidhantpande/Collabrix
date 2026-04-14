0edbffe56ca52aaa7ad55639a05082ceba65c603
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-14 19:01:08 -0300

    feat(notification-service): add notification response dto

--- notification-service/src/main/java/com/mobflow/notificationservice/model/dto/response/NotificationResponseDTO.java
+++ notification-service/src/main/java/com/mobflow/notificationservice/model/dto/response/NotificationResponseDTO.java
@@ -1,4 +1,18 @@
 package com.mobflow.notificationservice.model.dto.response;
 
-public record NotificationResponseDTO() {
-}
+import com.mobflow.notificationservice.model.enums.NotificationChannel;
+import com.mobflow.notificationservice.model.enums.NotificationPriority;
+import com.mobflow.notificationservice.model.enums.NotificationType;
+
+import java.time.Instant;
+import java.util.Map;
+
+public record NotificationResponseDTO(
+        String id,
+        NotificationType type,
+        NotificationPriority priority,
+        boolean read,
+        Instant createdAt,
+        Instant readAt,
+        Map<String, String> metadata
+){}
