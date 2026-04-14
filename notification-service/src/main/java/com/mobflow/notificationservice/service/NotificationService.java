b06243c462bdd48aea34092c37fdaba5ca6c1210
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-14 19:01:08 -0300

    feat(notification-service): implement notification service

--- notification-service/src/main/java/com/mobflow/notificationservice/service/NotificationService.java
+++ notification-service/src/main/java/com/mobflow/notificationservice/service/NotificationService.java
@@ -1,4 +1,17 @@
 package com.mobflow.notificationservice.service;
 
+import com.mobflow.notificationservice.model.entities.Notification;
+import com.mobflow.notificationservice.repository.NotificationRepository;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@NoArgsConstructor
 public class NotificationService {
+    private NotificationRepository notificationRepository;
+
+    public Notification createNotification(Notification notification) {
+
+        return notificationRepository.save(notification);
+    }
 }
