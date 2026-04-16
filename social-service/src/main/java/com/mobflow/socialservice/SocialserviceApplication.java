5a33cccc066ca79330afa893cf041d1d16f62543
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-16 15:15:55 -0300

    feat(social-service): bootstrap Spring Boot service

---
+++ social-service/src/main/java/com/mobflow/socialservice/SocialserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mobflow.socialservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialserviceApplication.class, args);
+	}
+
+}
---
+++ social-service/src/test/java/com/mobflow/socialservice/SocialserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobflow.socialservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
