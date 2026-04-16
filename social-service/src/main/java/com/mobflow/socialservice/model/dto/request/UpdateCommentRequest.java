4c495ea6b41dac4de3fe7cc7b65c40f1c5441e96
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-16 15:48:31 -0300

    feat(social-service): add comment request contracts

---
+++ social-service/src/main/java/com/mobflow/socialservice/model/dto/request/UpdateCommentRequest.java
@@ -0,0 +1,15 @@
+package com.mobflow.socialservice.model.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateCommentRequest {
+
+    @NotBlank(message = "content is required")
+    @Size(max = 4000, message = "content must have at most 4000 characters")
+    private String content;
+}
