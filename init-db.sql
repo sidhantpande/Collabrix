af4c6e1694ed723a790b4d189fd3e8f82aa8c82c
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-17 22:03:55 -0300

    chore(config): align local env template and postgres init script

--- init-db.sql
+++ init-db.sql
@@ -1,4 +1,20 @@
-CREATE DATABASE mobflow_auth;
-CREATE DATABASE mobflow_user;
-CREATE DATABASE mobflow_workspace;
-CREATE DATABASE mobflow_task;
+-- Databases required by the relational Mobflow services.
+SELECT 'CREATE DATABASE mobflow_auth'
+WHERE NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'mobflow_auth'
+)\gexec
+
+SELECT 'CREATE DATABASE mobflow_user'
+WHERE NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'mobflow_user'
+)\gexec
+
+SELECT 'CREATE DATABASE mobflow_workspace'
+WHERE NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'mobflow_workspace'
+)\gexec
+
+SELECT 'CREATE DATABASE mobflow_task'
+WHERE NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'mobflow_task'
+)\gexec
